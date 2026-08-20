75767ae46610ef60fe6fe6e62d6f9aefeeb7f8b8
Yaswanth Kiran Devarasetti <yaswanth.devarasetti@acuvate.com>
2026-08-20 15:02:12 +0530

    String Dictionary Order Qn

--- DictionaryOrder.py
+++ DictionaryOrder.py
@@ -1,7 +1,18 @@
-str= input("Enter the String: ").lower()
-words = str.split(" ")
-first = words[0]
-for word in words:
-    if(word < first):
-        first = word
-print(first)
+# str= input("Enter the String: ").lower()
+# words = str.split(" ")
+# first = words[0]
+# for word in words:
+#     if(word < first):
+#         first = word
+# print(first)
+
+
+# str = input("Enter the String: ").lower()
+# words = str.split(" ")
+# words.sort()
+# print(words[0])
+
+# text = input("Enter the String: ")
+# words = text.split()
+# words.sort(key = str.upper)
+# print(words[0]) 
