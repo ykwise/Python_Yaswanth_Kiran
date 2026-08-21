5e4d42b39a4f6f73a70a96706738312f69d2d09c
Yaswanth Kiran Devarasetti <yaswanth.devarasetti@acuvate.com>
2026-08-21 10:33:34 +0530

    List_Tuple_qns

---
+++ LIst_Tuples/qn1.py
@@ -0,0 +1,48 @@
+#1st question
+list_a = [1,2,3,4,5]
+list_a.append(6)
+print(list_a)
+
+#2nd question
+names = ["yaswanth","abhi","jagadeesh"]
+names.insert(2,"Rahul")
+print(names)
+
+#3rd question
+ls = [10,20,30,20,40,20]
+ls.remove(20)
+print(ls)
+
+#4th question
+list_4 = [5,2,8,1,9]
+list_4.sort()
+list_4b = sorted(list_4,reverse = True)
+print(list_4)
+print(list_4b)
+
+#5th question
+list_5 =  [10, 20, 30, 40, 50]
+print(list_5.pop())
+print(list_5)
+
+#6th question
+list_6 = [1, 2, 2, 3, 2, 4]
+print(list_6.count(2))
+
+#7th question
+list_7 = ["apple", "banana", "mango", "orange"]
+print(list_7.index("mango"))
+
+#8th question
+list_8a = [1,2,3,4,5]
+list_8b = [6,7,8,9,10]
+list_8a.extend(list_8b)
+print(list_8a)
+
+#9th question
+tuple_9th = (10, 20, 10, 30, 10, 40)
+print(tuple_9th.count(10))
+
+#10th question
+tuple_10th = ("Python", "Java", "C++", "JavaScript")
+print(tuple_10th.index("C++"))
